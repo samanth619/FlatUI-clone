338847cbcd550fff5e13ef4b43d1dea7d3415b0a
Samanth619 <samanthreddy619@gmail.com>
2021-06-20 21:55:06 +0530

    Generating various shades of each color using "chroma-js library"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
-import Palette from './Palette'
-import seedColors from './seedColors'
+import Palette from './Palette';
+import seedColors from './seedColors';
+import {generatePalette} from './colorHelper.js';
 
-function App() {
+class App extends Component{
+  render(){
+    console.log(generatePalette(seedColors[4]));
   return (
     <div className="App">
       <Palette {...seedColors[4]}/>
     </div>
   );
+  }
 }
 
 export default App;
