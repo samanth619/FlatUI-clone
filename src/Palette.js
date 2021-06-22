2b5675a8a446768e7c3f9352cdee34724405c26c
Samanth619 <samanthreddy619@gmail.com>
2021-06-22 12:41:21 +0530

    Adding Navbar to Palette component

--- src/Palette.js
+++ src/Palette.js
@@ -20,7 +20,7 @@ class Palette extends Component {
     }
 
     render() {
-        const {colors} = this.props.palette;
+        const {colors,paletteName,emoji} = this.props.palette;
         const {level,format} = this.state;
         const colorBoxes = colors[level].map(color=>(
             <ColorBox key={color.id} background={color[format]} name={color.name} />
@@ -29,6 +29,10 @@ class Palette extends Component {
             <div className="Palette">
               <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
               <div className="Palette-colors">{colorBoxes}</div>
+              <footer className="palette-footer">
+                  {paletteName}
+                  <span className="emoji">{emoji}</span>
+              </footer>
             </div>
         );
     }
